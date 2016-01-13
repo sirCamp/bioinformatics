732dc692c8217095d8d0131089407a25f2f183b7
sirCamp <sircampydevelop@gmail.com>
2016-01-13 14:27:43 +0100

    stgandard deviation

--- algorithm/FirstAlgorithm.py
+++ algorithm/FirstAlgorithm.py
@@ -1,4 +1,5 @@
 from base.BaseAlgorithm import BaseAlgorithm
+import math
 
 class FirstAlgorithm(BaseAlgorithm):
 
@@ -7,15 +8,45 @@ class FirstAlgorithm(BaseAlgorithm):
 
     def run(self):
         genome_lenght  = 0
+        data = []
+        sum = 0;
+        lineNumber = 0;
         with open(self.path) as file:
             for line in file:
                 genome_lenght += 1
-                if genome_lenght == 7:
-                    exit(0)
+                #if genome_lenght == 7:
+                #    exit(0)
 
                 if line.find("@") != -1:
                     continue;
 
                 pieces = line.split("\t")
-                print len(pieces)
 
+                seven = 0;
+                three = 0;
+
+                for index, item in enumerate(pieces, start=0):   # default is zero
+                    if index == 7:
+                        seven = int(item)
+
+                    if index == 3:
+                        three = int(item)
+
+                temp = abs(three - seven);
+                lineNumber += 1
+                sum +=temp
+                #print str(sum) +" "+str(temp)
+                data.append(temp)
+
+        med = sum/lineNumber
+
+        sum = 0
+        lineNumber = 0
+        for index, item in enumerate(data, start=0):
+            lineNumber = index
+            sum += pow((med-item),2);
+
+
+        print "media: "+str(med)
+        print "varianza: "+str(sum/lineNumber)
+        print "variazione standard: "+str(math.sqrt(sum/lineNumber))
